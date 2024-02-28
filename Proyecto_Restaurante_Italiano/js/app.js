34453b7cb658152e3bb880c5d5153603cbdd7449
Miguel Orozco <miguelorozcob@hotmail.com>
2024-02-28 15:50:03 -0500

    modificación proyecto restaurante italiano(menu hamburguesa y boton de cerrar menu)

--- Proyecto_Restaurante_Italiano/js/app.js
+++ Proyecto_Restaurante_Italiano/js/app.js
@@ -1,18 +1,59 @@
 const menu = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+
 
 document.addEventListener('DOMContentLoaded', ()=>{
-
+    eventos();
 });
 
-//hoisting
+const eventos = () =>{
+    menu.addEventListener('click', abrirMenu);
+}
 
+const abrirMenu = () =>{
+    navegacion.classList.remove('ocultar')
+    botonCerrar();
+}
 
+const botonCerrar = () =>{
+    const btnCerrar = document.createElement('P');
+    const overlay = document.createElement('div');
+    overlay.classList.add('pantalla-completa');
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    btnCerrar.textContent = 'x';
+    btnCerrar.classList.add('btn-cerrar');
+    navegacion.appendChild(btnCerrar);
+    cerrarMenu(btnCerrar, overlay);
+}
 
-//arrow function o function expression
-const sumar2 = () =>{
-    console.log('arrow function',3+3);
+const cerrarMenu = (boton, overlay) =>{
+    boton.addEventListener('click', ()=>{
+        navegacion.classList.add('ocultar');
+        overlay.remove();
+    })
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //LINK DEL VIDEO: https://www.youtube.com/watch?v=QUCJ_CpkFbo&list=PLarV53MJTFV39oxNJScsqTNYwBQzRs-MJ&ab_channel=AzaelW3b
-//TIEMPO: 1:00:00
+//TIEMPO: 1:27:00
