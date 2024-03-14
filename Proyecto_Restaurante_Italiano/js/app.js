d33c858e1a185a3ded302c5e50a39fb7f91a64e5
Miguel Orozco Buendia <miguelorozcob@hotmail.com>
2024-03-13 22:19:07 -0500

    agregando funcionalidades a la hamburguesa, y optimizando la pagina para evitar que los botones se ejecuten constantemente

--- Proyecto_Restaurante_Italiano/js/app.js
+++ Proyecto_Restaurante_Italiano/js/app.js
@@ -20,9 +20,15 @@ const botonCerrar = () =>{
     const overlay = document.createElement('div');
     overlay.classList.add('pantalla-completa');
     const body = document.querySelector('body');
+    if(document.querySelectorAll('.pantalla-completa').length > 0) return;
     body.appendChild(overlay);
     btnCerrar.textContent = 'x';
     btnCerrar.classList.add('btn-cerrar');
+    // while(navegacion.children[5]){
+    //     navegacion.removeChild(navegacion.children[5]);
+    // }
+
+
     navegacion.appendChild(btnCerrar);
     cerrarMenu(btnCerrar, overlay);
 }
@@ -31,7 +37,13 @@ const cerrarMenu = (boton, overlay) =>{
     boton.addEventListener('click', ()=>{
         navegacion.classList.add('ocultar');
         overlay.remove();
-    })
+        boton.remove();
+    });
+
+    overlay.onclick =function(){
+        overlay.remove();
+        navegacion.classList.add('ocultar');
+    }
 }
 
 
